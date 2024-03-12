f6c0616cd42b87ef5e47ffd20469b42f53b6dd68
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-03-12 12:45:47 +0100

    inserimento sezione critics

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -46,6 +46,65 @@
     </div>
   </div>
 
+  <div class="critics">
+    <div class="critics_left">
+      <h4>
+        What Critics Say
+      </h4>
+      <h6>
+        testimonials from around the world
+      </h6>
+      <div class="bestAuthor">
+        <h4>
+          Best Author Of His Generation
+        </h4>
+        <p>
+          This book will change your perspective on life
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti repellendus asperiores laborum sit officia possimus fugiat veniam aliquid error tenetur, temporibus saepe voluptatum facere, maxime commodi rem eligendi, debitis incidunt.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore ut porro, alias ea totam atque debitis sapiente accusantium fugit nulla cupiditate ipsam numquam quod aperiam consectetur. Nostrum natus facilis sed?
+        </p>
+        <div class="source">
+          <img src="" alt="Logo The New York Times">
+          <h6>Justine Kilpatrick, <span>The New York Times</span></h6>
+        </div>
+      </div>
+    </div>
+
+    <div class="critics_right">
+      <button type="button">
+        Read All Testimonials
+      </button>
+      <div class="review">
+        <h5>
+          A True Masterpiece, Bravo!
+        </h5>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima maiores autem ipsum deserunt eaque qui hic accusantium tempore laboriosam ex magni, nam incidunt, obcaecati quo ipsa laudantium quisquam. Atque, nostrum.
+        </p>
+      </div>
+      <div class="source_2">
+        <img src="" alt="Logo The Guardian">
+        <h6>Gerald Hendley, The Guardian</h6>
+      </div>
+      <div class="review">
+        <h5>
+          A Unique View On The World
+        </h5>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima maiores autem ipsum deserunt eaque qui hic accusantium tempore laboriosam ex magni, nam incidunt, obcaecati quo ipsa laudantium quisquam. Atque, nostrum.
+        </p>
+      </div>
+      <div class="source_2">
+        <img src="" alt="Logo The Globe And Mail">
+        <h6>Mary Maxey, The Globe And Mail</h6>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <style lang="scss">
@@ -147,6 +206,10 @@
         
       }
     }
+  }
+
+  .critics {
+    max-width: 692px;
   }
 
   
