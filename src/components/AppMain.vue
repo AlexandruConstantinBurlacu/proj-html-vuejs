e9563cae1a52968fa70ddaf1500840312397a54d
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-03-12 12:35:07 +0100

    inserimento sezione new audiobook

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -16,6 +16,36 @@
       Meet The Author
     </button>
   </div>
+
+  <div class="avaliable">
+    <ul>
+      <li>ebay</li>
+      <li>audible</li>
+      <li>barnes</li>
+      <li>kindle</li>
+    </ul>
+  </div>
+
+  <div class="newAudiobook">
+    <img src="../assets/images/audible-app-2x-400x418.png" alt="Audible">
+    <div class="new">
+      <h2>
+        New AudioBook
+      </h2>
+      <h4>
+        no time to read? no problem. listen to it
+      </h4>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      </p>
+      <ul>
+        <li>audible</li>
+        <li>barnes</li>
+        <li>kindle</li>
+      </ul>
+    </div>
+  </div>
+
 </template>
 
 <style lang="scss">
@@ -49,6 +79,73 @@
       border: 0px;
       border-radius: 6px;
       cursor: pointer;
+    }
+  }
+
+  .avaliable {
+    background-color: #fef7e9;
+
+    ul {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      gap: 74px;
+      list-style-type: none;
+      padding-top: 63px;
+      padding-bottom: 50px;
+
+      li {
+        font-size: 20px;
+      }
+    }
+  }
+
+  .newAudiobook * {
+    color: white;
+  }
+
+  .newAudiobook {
+    max-height: 404px;
+    background-image: url("../assets/images/banner.jpg");
+    background-size: cover;
+    background-position: center;
+    display: flex;
+    justify-content: center;
+    gap: 71px;
+
+    img {
+      margin-top: 48px;
+      width: 304px;
+      height: auto;
+    }
+
+    .new {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      margin-top: 136px;
+      margin-bottom: 109px;
+
+      h4 {
+        color: #f4b429;
+        text-transform: uppercase;
+        margin-top: 21px;
+        margin-bottom: 28px;
+      }
+
+      p {
+        color: #c6c4c4;
+        margin-bottom: 40px;
+      }
+
+      ul {
+        display: flex;
+        flex-direction: row;
+        gap: 32px;
+        list-style-type: none;
+        
+      }
     }
   }
 
